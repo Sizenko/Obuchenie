49a94058596f29b6fa212badca86b41107d1e7c0
sizenko.tlt@mail.ru <sizenko.tlt@mail.ru>
2023-08-30 01:11:56 +0400

    12

---
+++ praktika_3_2.py
@@ -0,0 +1,9 @@
+print("Назовите по порядку этапы эволюции человека на латинском языке")
+a=input("Первый этап ")
+b=input("Второй этап ")
+c=input("Третий этап ")
+d=input("Четвёртый этап ")
+f=input("Пятый этап ")
+print(a,b,c,d,f,sep="=>")
+print("Правильный ответ","Dryopithecus","Australopithecus","Homo erectus","Homo sapiens","Homo sapien sapiens",sep="=>")
+ 
